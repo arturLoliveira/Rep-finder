8985f625f69a14c1e01329e1e4d6f8ca73cdc20a
Artur Linhares de Oliveira <arturoliveira@outlook.com.br>
2025-05-07 22:25:59 -0300

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -8,8 +8,8 @@ const firebaseConfig = {
   authDomain: "finder-798c6.firebaseapp.com",
   projectId: "finder-798c6",
   storageBucket: "finder-798c6.firebasestorage.app",
-  messagingSenderId: "563927585752",
-  appId: "1:563927585752:web:ff4dbc229d9e40b5b358ad",
+  messagingSenderId: "",
+  appId: "",
   measurementId: "G-8Z2CBXL3BN"
 };
 const app = initializeApp(firebaseConfig);
